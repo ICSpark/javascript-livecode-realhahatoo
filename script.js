03571036e97e38ddd4f912c66161e23bfcc1f5f1
JeremiahS13 <148928125+realhahatoo@users.noreply.github.com>
2025-02-08 19:54:03 +0000

    Practicing JS

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("helloworld");
+console.log("Hello World");
+var Money1 = 69
+var Money2 = -1000
+
+
+if (Money1 < 100) {
+    console.log("You are in poor");
+}
+else {
+    console.log("This person is richhh!!!!!");
+}
+
